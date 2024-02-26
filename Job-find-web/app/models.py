5d96ec462cdf6f87b56ba246a72a6d63022a9f11
amul patle <amulpatle3@gmail.com>
2024-02-26 07:21:56 +0530

    create models

---
+++ Job-find-web/app/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+# Create your models here.
+
+class UserMaster(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    otp = models.IntegerField()
+    role = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_varified = models.BooleanField(default=False)
+    is_created = models.DateTimeField(auto_now_add=True)
+    is_updated = models.DateTimeField(auto_now_add=True)
+    
+    
+class Candidate(models.Model):
+    user_id = models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    dob = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50)
+    profile_pic = models.ImageField(upload_to="app/img/candidate")
+    
+class Company(models.Model):
+    user_id = models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=150)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    logo_pic = models.ImageField(upload_to="app/img/company/")
+    
